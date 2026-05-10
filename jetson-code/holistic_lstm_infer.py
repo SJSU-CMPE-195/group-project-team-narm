050e3628828b3ee30ecfa604d994e0d519bed671
Newton Tran <newtontran0421@gmail.com>
2026-05-09 20:38:13 -0700

    lower MediaPipe Holistic model_complexity to 0 for Nano demo speedup

--- jetson-code/holistic_lstm_infer.py
+++ jetson-code/holistic_lstm_infer.py
@@ -67,6 +67,7 @@ class HolisticLSTMInfer:
         self._holistic = self._mp_holistic.Holistic(
             min_detection_confidence=min_detection_confidence,
             min_tracking_confidence=min_tracking_confidence,
+            model_complexity=0,
         )
 
         self._sequence: List[np.ndarray] = []
